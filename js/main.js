857fe6db508695b6f8e6a6c2a4d88140212af161
unknown <miamby111@gmail.com>
2023-02-22 21:46:55 +0200

    adding

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 const navBtn = document.querySelector(".nav__toggle");
 const nav = document.querySelector(".nav");
 const menuIcon = document.querySelector(".menu-icon");
-
+console.log("nav", nav);
 navBtn.onclick = function () {
   nav.classList.toggle("nav--mobile");
   menuIcon.classList.toggle("menu-icon--active");
